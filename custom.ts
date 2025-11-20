6eaf78efd334d03197fb3861126924e2cf4fd602
10-biter <ep.student@hotmail.com>
2025-11-20 22:20:16 +0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -14,7 +14,7 @@ enum MyEnum {
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon=""
+//% weight=100 color=#606e57 icon="\f005"
 namespace custom {
     /**
      * TODO: describe your function here
